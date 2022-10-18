5cef786adee66949a2c1045d8991197782f3300d
Jeonghoon Jason Choi <jchoi00@bu.edu>
2022-10-18 18:31:27 -0400

    Update MIDASplatemap.py 10.18.2022
    Update to add Plate Class draft
     - creates object
     - needs debugging of Well Class to see why the automatic creation of empty plate is erroring

--- MIDASplatemap.py
+++ MIDASplatemap.py
@@ -13,9 +13,10 @@ import pandas as pd
 import matplotlib
 import string
 
-defaultwell = {'sample_name','row','column','category', 'index'}
-wellfields = {'pcr' : {'volume', 'unit'},'gdna' : {'volume', 'unit'}, 'kingfisher' : {}, 'postpcr' : {}, 'quantit' : {'dilution_factor','volume', 'unit'}}
-# platefields = {'pcr' : {'mastermix'},'gdna' : {}, 'kingfisher' : {'sampletype'}, 'postpcr' : {}, 'quantit' : {}}
+defaultwell = {'sample_name','row','column','index','injection'}
+defaultplate = {'screen','date'}
+wellfields = {'pcr' : {'volume', 'unit'},'gdna' : {'volume', 'unit','concentration'}, 'kingfisher' : {}, 'postpcr' : {}, 'quantit' : {'dilution_factor','volume', 'unit'}}
+platefields = {'pcr' : {'mastermix'},'gdna' : {}, 'kingfisher' : {'sampletype'}, 'postpcr' : {}, 'quantit' : {}}
 
 def _getfields(classname, category):
     """Get the fields/keys of class object for object initialization.
@@ -28,14 +29,13 @@ def _getfields(classname, category):
     """
     if classname == 'Well':
         fields = defaultwell
-        fields.update(wellfields[category])
-        return fields
-    # else classname =='Plate':
-    #     fields = defaultplate
-    #     fields = fields.update(platefields[category])
-        
-    
-        
+        if category is not None:
+            fields.update(wellfields[category])
+    elif classname =='Plate':
+        fields = defaultplate
+        if category is not None:
+            fields.update(platefields[category])
+    return fields
 
 class Well:
     """Class for Well objects.
@@ -48,7 +48,7 @@ class Well:
     """
     
     def __init__(self, category):
-        """Initiation object of Well object
+        """Initiation object of Well class
         
             Args:
                 _category (str): parameter that determines the loading of category dependent fields
@@ -62,22 +62,35 @@ class Well:
             default[name] = None
         self._fields = default
         self._category = category
+    
+    def __setfield__(self,name, value):
+        """Function to set values of attributes within the field attribute of Well class
         
-    def __getattr__(self, name):
+            Args:
+                name: The field name
+                value: The value for the field
+        """
+        
+        if name in self._fields.keys():
+            self._fields[name] = value
+        else:
+            raise TypeError('Field does not exist in Well object')
+        
+    def __getfield__(self, name):
         """Overwrites the attribute getter default function
 
             Args:
                 name: The field name
                 
             Returns:
-                The value contained in the specified field
+                The value contained in the field
 
         """
         
         if name in self._fields.keys():
             return self._fields[name].value
         else:
-            return TypeError('Field does not exist in Well object')
+            raise TypeError('Field does not exist in Well object')
         
     def get_row(self):
         """Returns the value of a the 'row' field of a Well class object
@@ -114,22 +127,161 @@ class Well:
         """
         
         return self._fields['sample_name']
-      
+    
+    def df_return(self):
+        """Returns the Well object information organized in a pandas DataFrame format
+        
+            Returns:
+                DataFrame object with information organized
+        """
+        df = pd.DataFrame([self._fields])
+        return df
         
 class Plate:
     """Class for Plate objects.
     
     Generate a structure to handle plates and contents of plate and according metadata.
     
-    Default Attributes:
-        screen (str): unique identification string for screen
-        size (int): number of wells in plate
-        date (str): date of plate creation/sample processing
-        content (str): content of plate - determines the nondefault field creation
+    Attributes:
+        _category (str): content of plate - determines the nondefault field creation
+        _fields (dict): dictionary of field values of well depending on category 
+        _size (int): number of wells in plate
+        _wells (list(Well)): 2D list of Well objects that correspond to plate dimensions
     """
+    def __init__(self, category, size):
+        """Initiation object of Plate class
+        
+            Args:
+                _category (str): content of plate - determines the nondefault field creation
+                _fields (dict): dictionary of field values of well depending on category 
+                _size (int): number of wells in plate
+                _wells (list(Well)): 2D list of Well objects that correspond to plate dimensions
+        """
+        defaultlist = _getfields('Plate', category)
+        default = {}
+        for name in defaultlist:
+            default[name] = None
+        self._fields = default
+        self._category = category
+        self._size = size
+        dim = {6:(2,3), 12:(3,4), 24:(4,6), 96: (8,12), 384: (16,24)}
+        if size not in dim.keys():
+            self._wells = None
+            raise TypeError('Invalid Plate Size')
+        else: 
+            wells = [pd.DataFrame(columns=['row','column','index'])]*size
+            letters = list(string.ascii_uppercase)
+            # define rows (note wells defined earlier)
+            rows = letters[0:(dim[size])[0]]
+            # list of cell letters
+            rowindex = rows*(dim[size])[1]
+            # sorting EITHER rows or columns lists the well ID's in the correct order
+            rowindex.sort()
+            
+            # define the correct number of columns according to the well plate
+            columns = list(range(1, (dim[size])[1]+1))
+            # list of cell numbers for every well
+            columnindex = columns*(dim[size])[0]
+            # dictionary of cell letters (rows) and numbers (columns)
+            cellsdict = {'row':rowindex, 'column':columnindex}
+            df = pd.DataFrame(cellsdict)
+            
+            #working on debugging creating an automatically indexed plate... stuck here. saving wrong indices.
+            for i in range(len(wells)):
+                rowval = df['row'][i]
+                colval = df['column'][i]
+                wells[i]['row']=( rowval)
+                wells[i]['column']=(colval)
+                wells[i]['index']=rowval+colval.astype(str)
+                
+            
+            self._wells = wells
+
+        
+    def __getfield__(self, name):
+        """Overwrites the attribute getter default function
+
+            Args:
+                name: The field name
+                
+            Returns:
+                The value contained in the field
+
+        """
+        
+        if name in self._fields.keys():
+            return self._fields[name].value
+        else:
+            raise TypeError('Field does not exist in Plate object')
+        """Overwrites the attribute getter default function
     
+            Args:
+                name: The field name
+                
+            Returns:
+                The value contained in the field
+    
+        """
+        if name in self._fields.keys():
+            return self._fields[name].value
+        else:
+            raise TypeError('Field does not exist in Well object')
+    def get_category(self):
+        """Returns the category attribute of the Plate class object
+        
+            Returns:
+                The string the describes the category of the content in the plate.
+        """
+        """Returns the category the palte is classified as
+        
+            Returns:
+                String of category name
+        """
+        return self._category
+        return self.category
+    def get_size(self):
+        """Returns the number of wells within the plate
+        
+            Returns:
+                Integer value of number of wells in plate
+        """
+        
+        return(self._size)
+        """Returns the size attribute of the Plate class object
+        
+            Returns:
+                The integer value of the number of wells within plate object
+        """
+    def print_platemap(self, attr = 'sample_name'):
+        """Displays specified information of all Well objects in Plate as a pandas DataFrame format organized in the platemap dimensions.
+        
+            Returns:
+                DataFrame object with information organized
+        """
+        dim = {6:(2,3), 12:(3,4), 24:(4,6), 96:(8,12), 384:(16,24)} 
+        attr_df = [0]*len(self._wells)
+        for i in range(len(self._wells)):
+            attr_df[i] = self._wells[i]._fields[attr]
+        reshaped = np.asarray(attr_df).reshape(dim[self._size])
+        df = pd.DataFrame(reshaped)
+        return df
+    # def printinfo_tabular(self, attr = 'sample_name'):
+    #     """Displays specified information of all Well objects in Plate as a linear tabular DataFrame format.
+        
+    #         Args:
+    #             attr (str): string of attribute name
+    #     """
+        
+        
+    # def printall_tabular():
+                
 def main():
     hello = Well('gdna')
+    df = (hello.df_return())
+    # print(df)
+    goodbye = Plate('gdna',size = 96)
+    df2 = goodbye.print_platemap('index')
+    # print(goodbye)
     return 0
 
 main()
